a3fef69bec1741b88a785626541def772167edcb
Todd Holford <toddholford1996@gmail.com>
2020-12-07 00:41:01 -0600

    added player, enemy, and tilemap

---
+++ PROTO-Simple-2D-Game/Assets/playerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+
+    [SerializeField]
+    Vector2 playerInput;
+    Rigidbody2D playerRigidbody;
+
+    [SerializeField]
+    float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        playerInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        playerRigidbody.velocity = playerInput.normalized * moveSpeed;
+    }
+}
